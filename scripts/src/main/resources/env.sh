de461f3850fb90ece61ad707bef32e904c27f5dc
Jörg Hohwiller <hohwille@users.sourceforge.net>
2018-12-19 16:34:01 +0100

    #21: improve OS support
    #23: proper fix for dirname of sourced script

--- scripts/src/main/resources/env.sh
+++ scripts/src/main/resources/env.sh
@@ -1,11 +1,6 @@
-#!/bin/bash
-#
-# Source this script from a BASH shell to setup the shell for the corresponding project.
-# 
+# Source this script from a linux shell to setup the environment variables for the corresponding project.
+pushd "$(dirname "${BASH_SOURCE:-$0}")" >/dev/null
 
-pushd "$(dirname "$0")" >/dev/null
-export OASP_PROJECT_HOME=`pwd`
-
-. $OASP_PROJECT_HOME/scripts/environment-project
+. $PWD/scripts/environment-project
 
 popd > /dev/null
