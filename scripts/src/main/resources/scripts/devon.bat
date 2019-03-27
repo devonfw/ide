e56bdf909bcbcb0865e089623c62ef52fc48f6ce
Jörg Hohwiller <hohwille@users.noreply.github.com>
2019-03-27 10:23:22 +0100

    windows fix (#51)
    * #49: proper fix for BAT world
    
    * #49: proper fix for bash world
    
    * small fix for archetype

--- scripts/src/main/resources/scripts/devon.bat
+++ scripts/src/main/resources/scripts/devon.bat
@@ -5,7 +5,6 @@ if "%1%" == "-v" (
 if "%1%" == "--version" (
   goto :printVersion
 )
-pushd %CD%
 
 rem Auto-install oursevles...
 if not exist "%USERPROFILE%\scripts" (
@@ -28,18 +27,68 @@ if not exist "%USERPROFILE%\scripts\devon.bat" (
       set "PATH=%PATH%;%USERPROFILE%\scripts"
     )
   )
-  echo The devon CLI script has been installed to your system.
+  echo The devon CLI script has been installed to your windows system.
   echo Now in any new command shell, you can call devon to setup your IDE enviromennt variables.
   echo You can also provide arguments to devon for advanced usage, e.g. try calling 'devon help'
 )
 
+if "%1%" == "" (
+  goto :setup_env
+)
+if exist "%ProgramFiles%\Git\bin\bash.exe" (
+  set "BASH=%ProgramFiles%\Git\bin\bash.exe"
+  set "HOME=%USERPROFILE%"
+  goto :bash_detected
+)
+if exist "%ProgramFiles(x86)%\Git\bin\bash.exe" (
+  set "BASH=%ProgramFiles(x86)%\Git\bin\bash.exe"
+  set "HOME=%USERPROFILE%"
+  goto :bash_detected
+)
+if exist "C:\cygwin64\bin\bash.exe" (
+  set "BASH=C:\cygwin64\bin\bash.exe"
+  set "HOME=C:\cygwin64\home\%USERNAME%"
+  goto :bash_detected
+)
+if exist "C:\cygwin\bin\bash.exe" (
+  set "BASH=C:\cygwin\bin\bash.exe"
+  set "HOME=C:\cygwin\home\%USERNAME%"
+  goto :bash_detected
+)
+echo:
+echo *** ATTENTION ***
+echo Bash has not been found on your system!
+echo Please install git for windows on your system.
+echo https://git-scm.com/download/win
+goto :end
+
+:bash_detected
+if not "%DEVON_OLD_PATH%" == "" (
+  set "DEVON_PATH=%PATH%"
+  set "PATH=%DEVON_OLD_PATH%"
+  set "DEVON_OLD_PATH="
+)
+if not exist "%HOME%\.devon\devon" (
+  pushd %~dp0
+  "%BASH%" -c 'source devon'
+  popd
+)
+"%BASH%" -c 'source ~/.devon/devon %*'
+if not "%DEVON_PATH%" == "" (
+  set "DEVON_OLD_PATH=%PATH%"
+  set "PATH=%DEVON_PATH%"
+  set "DEVON_PATH="
+)
+
+:setup_env
 set "WORKSPACE=main"
+pushd %CD%
 :iterate_backwards
 if exist scripts\environment-project.bat (
   call scripts\environment-project.bat
   echo devon-ide environment variables have been set for %CD%
   popd
-  goto :cli
+  goto :end
 )
 if "%CD%" == "%CD:~0,3%" (
   popd
@@ -55,36 +104,6 @@ if "%folder%" == "workspaces" (
 )
 cd..  
 goto :iterate_backwards
-
-:cli
-if "%1%" == "" (
-  goto :end
-)
-set "BASH=%ProgramFiles(x86)%\Git\bin\bash.exe"
-set "HOME=%USERPROFILE%"
-if not exist "%BASH%" (
-  set "BASH=%ProgramFiles%\Git\bin\bash.exe"
-)
-if not exist "%BASH%" (
-  echo:
-  echo *** ATTENTION ***
-  echo Bash has not been found on your system!
-  echo Please install git for windows on your system.
-  goto :end
-)
-set "DEVON_PATH=%PATH%"
-set "PATH=%DEVON_OLD_PATH%"
-set "DEVON_OLD_PATH="
-if not exist "%HOME%\.devon\devon" (
-  pushd
-  cd "%DEVON_IDE_HOME%
-  "%BASH%" -c 'source scripts/devon'
-  popd
-)
-"%BASH%" -c 'source ~/.devon/devon %*'
-set "DEVON_OLD_PATH=%PATH%"
-set "PATH=%DEVON_PATH%"
-set "DEVON_PATH="
 
 goto :end
 
