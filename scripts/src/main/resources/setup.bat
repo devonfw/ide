fc5469e4e568de9f92ea29fd5d0b2eb4b34b251d
Jörg Hohwiller <hohwille@users.sourceforge.net>
2019-04-02 18:45:43 +0200

    #61: further improvements to console and windows explorer

--- scripts/src/main/resources/setup.bat
+++ scripts/src/main/resources/setup.bat
@@ -3,9 +3,10 @@
 pushd %~dp0
 
 call scripts\devon.bat ide setup
+call regedit.exe /s system/windows/cmd/devon-cmd.reg
+call regedit.exe /s system/windows/power-shell/devon-power-shell.reg
 
+echo Setup of devon-ide completed
 pause
 
 popd
-
-cmd
