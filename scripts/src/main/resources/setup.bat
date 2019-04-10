9196abb86ffe185207792aecbc8872791365cddc
Jörg Hohwiller <hohwille@users.noreply.github.com>
2019-04-11 00:03:04 +0200

    Feature/69 fix uninstall (#96)
    * #95: fixed by removing PS support for Background
    * #69: changed registry keys to HKCU as suggested my markusschuh
    * #69: fixed magic grep not found error
    * #61: added cygwin integration
    * #69: #50: uninstall both from cygwin and mingw
    * #97: bash fix and disabled settings update

--- scripts/src/main/resources/setup.bat
+++ scripts/src/main/resources/setup.bat
@@ -1,7 +1,7 @@
 @echo off
 
 pushd %~dp0
-
+echo Setting up your devon-ide in %CD%
 call scripts\devon.bat ide setup
 call regedit.exe /s system/windows/cmd/devon-cmd.reg
 call regedit.exe /s system/windows/power-shell/devon-power-shell.reg
