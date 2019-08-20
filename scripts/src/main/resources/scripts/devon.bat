97138ca4e209ab99bc5087f74b2d33d2990acf0b
Jörg Hohwiller <hohwille@users.sourceforge.net>
2019-08-20 13:16:11 +0200

    #200: update installed devon.bat if newer

--- scripts/src/main/resources/scripts/devon.bat
+++ scripts/src/main/resources/scripts/devon.bat
@@ -10,28 +10,38 @@ rem Auto-install oursevles...
 if not exist "%USERPROFILE%\scripts" (
   md "%USERPROFILE%\scripts"
 )
-if not exist "%USERPROFILE%\scripts\devon.bat" (
-  echo Copying devon CLI script to your home directory...
-  copy "%~f0" "%USERPROFILE%\scripts\devon.bat"
-  for %%f in (devon.bat) do set "p=%%~$PATH:f"
-  if not defined p (
-    echo Adding %USERPROFILE%\scripts to your users system PATH
-    for /F "tokens=2* delims= " %%f IN ('reg query HKCU\Environment /v PATH ^| findstr /i path') do set USER_PATH=%%g
-    if "%USER_PATH:~-1,1%" == ";" (
-      set "USER_PATH=%USER_PATH:~0,-1%"
-    )
-    setx PATH "%USER_PATH%;%%USERPROFILE%%\scripts"
-    if "%PATH:~-1,1%" == ";" (
-      set "PATH=%PATH%%USERPROFILE%\scripts"
-    ) else (
-      set "PATH=%PATH%;%USERPROFILE%\scripts"
-    )
+if exist "%USERPROFILE%\scripts\devon.bat" (
+  if not "%1%" == "ide" (
+    goto :devon_bat_installed
   )
-  echo The devon CLI script has been installed to your windows system.
-  echo Now in any new command shell, you can call devon to setup your IDE enviromennt variables.
-  echo You can also provide arguments to devon for advanced usage, e.g. try calling 'devon help'
+  if not "%2%" == "setup" (
+    goto :devon_bat_installed
+  )
+  echo "Updating devon.bat if newer"
+  xcopy /D /Y "%~f0" "%USERPROFILE%\scripts\devon.bat"
+  goto :devon_bat_installed
 )
+echo Copying devon CLI script to your home directory...
+copy "%~f0" "%USERPROFILE%\scripts\devon.bat"
+for %%f in (devon.bat) do set "p=%%~$PATH:f"
+if not defined p (
+  echo Adding %USERPROFILE%\scripts to your users system PATH
+  for /F "tokens=2* delims= " %%f IN ('reg query HKCU\Environment /v PATH ^| findstr /i path') do set USER_PATH=%%g
+  if "%USER_PATH:~-1,1%" == ";" (
+    set "USER_PATH=%USER_PATH:~0,-1%"
+  )
+  setx PATH "%USER_PATH%;%%USERPROFILE%%\scripts"
+  if "%PATH:~-1,1%" == ";" (
+    set "PATH=%PATH%%USERPROFILE%\scripts"
+  ) else (
+    set "PATH=%PATH%;%USERPROFILE%\scripts"
+  )
+)
+echo The devon CLI script has been installed to your windows system.
+echo Now in any new command shell, you can call devon to setup your IDE enviromennt variables.
+echo You can also provide arguments to devon for advanced usage, e.g. try calling 'devon help'
 
+:devon_bat_installed
 if "%1%" == "" (
   goto :setup_env
 )
