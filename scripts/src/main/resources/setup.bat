197d9f2c473b4d2e44a2ab496be2b19f9cf9a2d6
Jörg Hohwiller <hohwille@users.sourceforge.net>
2019-08-23 11:45:18 +0200

    #213: fixed cygwin

--- scripts/src/main/resources/setup.bat
+++ scripts/src/main/resources/setup.bat
@@ -7,10 +7,11 @@ reg import system/windows/cmd/devon-cmd.reg
 reg import system/windows/power-shell/devon-power-shell.reg
 
 for /F "usebackq tokens=2*" %%O in (`call "%SystemRoot%"\system32\reg.exe query "HKLM\Software\Cygwin\setup" /v "rootdir" 2^>nul ^| "%SystemRoot%\system32\findstr.exe" REG_SZ`) do set CYGWIN_HOME=%%P
-
 if exist "%CYGWIN_HOME%\bin\bash.exe" (
-  "%CYGWIN_HOME%\bin\bash.exe" -l -c "CD='%CD%';cd \"$CD\";./scripts/devon"
+  reg import system/windows/cygwin/devon-cygwin.reg
+  "%CYGWIN_HOME%\bin\bash.exe" -l -c "cd \"%CD%\";./scripts/devon"
 )
+
 echo Setup of devon-ide completed
 pause
 
