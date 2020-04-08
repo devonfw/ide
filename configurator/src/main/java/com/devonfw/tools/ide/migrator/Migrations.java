9f094ad2200239a860cabcc10b6f83b2c2bd4609
CORP\vapadwal <vaibhav.padwal@capgemini.com>
2020-04-08 21:59:50 +0530

    update migrator for version upgrade of spring boot

--- configurator/src/main/java/com/devonfw/tools/ide/migrator/Migrations.java
+++ configurator/src/main/java/com/devonfw/tools/ide/migrator/Migrations.java
@@ -172,12 +172,11 @@ public class Migrations {
         .next().to(VersionIdentifier.ofDevon4j("3.2.1")) //
         .pom().replaceProperty("devon4j.version", "3.2.1") //
         .and() //
-        .next().to(VersionIdentifier.ofDevon4j("3.3.0")) //
-        .pom().replaceProperty("devon4j.version", "3.3.0") //
+        .next().to(VersionIdentifier.ofDevon4j("2020.04.001")) //
+        .pom().replaceProperty("devon4j.version", "2020.04.001") //
         .replaceDependency(new VersionIdentifier("com.devonfw.java.modules", "devon4j-beanmapping", null),
-                new VersionIdentifier("com.devonfw.java.modules", "devon4j-beanmapping-dozer", null))
-        .and()
-        .next().build();
+            new VersionIdentifier("com.devonfw.java.modules", "devon4j-beanmapping-dozer", null))
+        .and().java().replace("new PageRequest", "PageRequest.of").replace("new Sort", "Sort.by").and().next().build();
   }
 
 }
