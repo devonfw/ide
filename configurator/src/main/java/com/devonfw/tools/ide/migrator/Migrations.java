674580d0f61d1b7373d3bf0fc6f6cb9fe9da7da9
Jörg Hohwiller <hohwille@users.sourceforge.net>
2020-12-14 12:35:19 +0100

    #498: added migration to devon4j 2020.12.001

--- configurator/src/main/java/com/devonfw/tools/ide/migrator/Migrations.java
+++ configurator/src/main/java/com/devonfw/tools/ide/migrator/Migrations.java
@@ -171,6 +171,11 @@ public class Migrations {
         .and() //
         .next().to(VersionIdentifier.ofDevon4j("2020.08.001")) //
         .pom().replaceProperty("devon4j.version", "2020.08.001") //
+        .and() //
+        .next().to(VersionIdentifier.ofDevon4j("2020.12.001")).pom() //
+        .replaceProperty("spring.boot.version", "2.3.3.RELEASE") //
+        .replaceProperty("jackson.version", "2.11.2") //
+        .replaceProperty("devon4j.version", "2020.12.001") //
         .and().next().build();
   }
 
