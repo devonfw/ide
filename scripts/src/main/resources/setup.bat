74a3f3e712d9a58b4aad3030a1220a5c39dc5bcf
Carsten Reitz <95033856+CREITZ25@users.noreply.github.com>
2022-05-23 18:25:09 +0200

    #701: Support for Windows Terminal (#725)

--- scripts/src/main/resources/setup.bat
+++ scripts/src/main/resources/setup.bat
@@ -7,6 +7,11 @@ call scripts\devon.bat ide setup %*
 reg import system/windows/cmd/devon-cmd.reg
 reg import system/windows/power-shell/devon-power-shell.reg
 
+dir %USERPROFILE%\AppData\Local\Microsoft\WindowsApps\wt.exe >NUL 2>NUL
+if "%ERRORLEVEL%" == "0" (
+  reg import system/windows/wt-cmd/devon-wt-cmd.reg
+)
+
 for /F "usebackq tokens=2*" %%O in (`call "%SystemRoot%"\system32\reg.exe query "HKLM\Software\Cygwin\setup" /v "rootdir" 2^>nul ^| "%SystemRoot%\system32\findstr.exe" REG_SZ`) do set CYGWIN_HOME=%%P
 if exist "%CYGWIN_HOME%\bin\bash.exe" (
   reg import system/windows/cygwin/devon-cygwin.reg
