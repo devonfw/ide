62450ca3e1167a3e176d602f732b01ae3308a032
Genetics <102921542+Amueller36@users.noreply.github.com>
2023-01-31 14:24:57 +0100

    Update setup.bat
    Added redirection to >nul 2>&1.

--- scripts/src/main/resources/setup.bat
+++ scripts/src/main/resources/setup.bat
@@ -6,37 +6,38 @@ set _RESET=[0m
 
 echo "%PSModulePath%" | findstr "%USERPROFILE%\Documents\WindowsPowerShell" >NUL
 if "%ERRORLEVEL%" == "0" (
-  echo Starting CMD window as workaround since setup has been called from powershell...
-  set PSModulePath=
-  start "CMD window" "%~f0"
-  goto :EOF
+echo Starting CMD window as workaround since setup has been called from powershell...
+set PSModulePath=
+start "CMD window" "%~f0"
+goto :EOF
 )
 
 pushd %~dp0
 echo Setting up your devonfw-ide in %CD%
 call scripts\devon.bat ide setup %*
-if %ERRORLEVEL% neq 0 ( 
-  exit /b %ERRORLEVEL%
+if %ERRORLEVEL% neq 0 (
+echo Error occurred while running devon.bat ide setup.
+exit /b %ERRORLEVEL%
 )
-reg import system/windows/cmd/devon-cmd.reg
-reg import system/windows/power-shell/devon-power-shell.reg
+reg import system/windows/cmd/devon-cmd.reg >nul 2>&1
+reg import system/windows/power-shell/devon-power-shell.reg >nul 2>&1
 
 dir %USERPROFILE%\AppData\Local\Microsoft\WindowsApps\wt.exe >NUL 2>NUL
 if "%ERRORLEVEL%" == "0" (
-  reg import system/windows/windows-terminal/devon-wt-cmd.reg
-  reg import system/windows/windows-terminal/devon-wt-gitbash.reg
-  reg import system/windows/windows-terminal/devon-wt-ps.reg
+reg import system/windows/windows-terminal/devon-wt-cmd.reg >nul 2>&1
+reg import system/windows/windows-terminal/devon-wt-gitbash.reg >nul 2>&1
+reg import system/windows/windows-terminal/devon-wt-ps.reg >nul 2>&1
 )
 
-for /F "usebackq tokens=2*" %%O in (`call "%SystemRoot%"\system32\reg.exe query "HKLM\Software\Cygwin\setup" /v "rootdir" 2^>nul ^| "%SystemRoot%\system32\findstr.exe" REG_SZ`) do set CYGWIN_HOME=%%P
+for /F "usebackq tokens=2*" %%O in (call "%SystemRoot%"\system32\reg.exe query "HKLM\Software\Cygwin\setup" /v "rootdir" 2^>nul ^| "%SystemRoot%\system32\findstr.exe" REG_SZ) do set CYGWIN_HOME=%%P
 if exist "%CYGWIN_HOME%\bin\bash.exe" (
-  reg import system/windows/cygwin/devon-cygwin.reg
-  "%CYGWIN_HOME%\bin\bash.exe" -l -c "cd \"%CD%\";./scripts/devon"
+reg import system/windows/cygwin/devon-cygwin.reg >nul 2>&1
+"%CYGWIN_HOME%\bin\bash.exe" -l -c "cd "%CD%";./scripts/devon"
 )
 
 echo %_fBGreen%Setup of devonfw-ide completed%_RESET%
 if not "%1%" == "-b" (
-  pause
+pause
 )
 
-popd
+popd	
