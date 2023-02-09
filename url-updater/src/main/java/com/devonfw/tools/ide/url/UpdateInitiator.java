6d722f0abcc35db8e0884bebf2f7f564b7a5c8e7
Genetics <102921542+Amueller36@users.noreply.github.com>
2023-02-09 09:19:16 +0100

    Pom.xml update
    Updated Pom.xml

--- url-updater/src/main/java/com/devonfw/tools/ide/url/UpdateInitiator.java
+++ url-updater/src/main/java/com/devonfw/tools/ide/url/UpdateInitiator.java
@@ -1,18 +1,14 @@
 package com.devonfw.tools.ide.url;
 
 import com.devonfw.tools.ide.url.updater.UpdateManager;
-import com.devonfw.tools.ide.url.folderhandling.UrlRepository;
-import com.devonfw.tools.ide.url.updater.java.JavaCrawler;
-
 
 import java.nio.file.Path;
 
 public class UpdateInitiator {
 
     public static void main(String[] args) {
-        String pathToRepo ="./target/test/UrlRepository";
+        String pathToRepo = args[0]
         UpdateManager updateManager = new UpdateManager(Path.of(pathToRepo));
         updateManager.updateAll();
-
     }
 }
