970e34a7b7a84d44dfed10eb19af1a67d8667516
Genetics <102921542+Amueller36@users.noreply.github.com>
2023-02-20 11:57:07 +0100

    Added missing import caused by sonarlift

--- url-updater/src/main/java/com/devonfw/tools/ide/url/folderhandling/UrlVersion.java
+++ url-updater/src/main/java/com/devonfw/tools/ide/url/folderhandling/UrlVersion.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Objects;
 
 
 import com.devonfw.tools.ide.url.folderhandling.abstractUrlClasses.AbstractUrlFolderWithParent;
