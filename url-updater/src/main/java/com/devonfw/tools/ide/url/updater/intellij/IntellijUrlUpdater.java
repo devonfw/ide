c09b936157df4cadc63cee7225eb47ebd5cbd198
diiinesh <63147290+diiinesh@users.noreply.github.com>
2023-06-01 11:26:24 +0200

    #1132 - implemented reading version, checksum from json provided by intellij
    Signed-off-by: diiinesh <63147290+diiinesh@users.noreply.github.com>

--- url-updater/src/main/java/com/devonfw/tools/ide/url/updater/intellij/IntellijUrlUpdater.java
+++ url-updater/src/main/java/com/devonfw/tools/ide/url/updater/intellij/IntellijUrlUpdater.java
@@ -1,63 +1,143 @@
 package com.devonfw.tools.ide.url.updater.intellij;
 
-import java.util.Set;
-import java.util.regex.Pattern;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.*;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-
+import com.devonfw.tools.ide.common.OperatingSystem;
+import com.devonfw.tools.ide.common.SystemArchitecture;
+import com.devonfw.tools.ide.json.mapping.JsonMapping;
+import com.devonfw.tools.ide.url.model.folder.UrlEdition;
+import com.devonfw.tools.ide.url.model.folder.UrlRepository;
+import com.devonfw.tools.ide.url.model.folder.UrlTool;
+import com.devonfw.tools.ide.url.model.folder.UrlVersion;
+import com.devonfw.tools.ide.url.updater.JsonUrlUpdater;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.devonfw.tools.ide.url.updater.WebsiteUrlUpdater;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
- * Abstract {@link WebsiteUrlUpdater} base class for IntelliJ.
+ *  {@link IntellijUrlUpdater} base class for IntelliJ.
  */
-public abstract class IntellijUrlUpdater extends WebsiteUrlUpdater {
-  @Override
-  protected Set<String> getVersions() {
+public class IntellijUrlUpdater extends JsonUrlUpdater<IntellijJsonObject> {
 
-    String url = getVersionUrl();
-    // TODO selenium seems overkill here
-    WebDriver driver = setupSelenium();
+  private final static String JSON_URL = "https://data.services.jetbrains.com/products?code=IIU%2CIIC&release.type=release&_=1682672979887";
+
+  private static final ObjectMapper MAPPER = JsonMapping.create();
+
+  private static final Logger logger = LoggerFactory.getLogger(IntellijUrlUpdater.class);
+
+  private boolean editionSwitch = false;
+
+  @Override
+  public void update(UrlRepository urlRepository) {
+
+    UrlTool tool = urlRepository.getOrCreateChild(getTool());
     try {
-      driver.get(url);
-      String html = driver.getPageSource();
-      Thread.sleep(5000);
-      driver.quit();
-      return doGetRegexMatchesAsList(html);
+      URL jsonURL = new URL(JSON_URL);
+      IntellijJsonObject[] jsonObj = MAPPER.readValue(jsonURL, IntellijJsonObject[].class);
+      // Has 2 elements, 1. Ultimate Edition, 2. Community Edition
+      for (int i = 0; i <= jsonObj.length - 1; i++) {
+        if (i == 1)
+          editionSwitch = true;
+        UrlEdition edition = tool.getOrCreateChild(getEdition());
+        updateExistingVersions(edition);
+        String toolWithEdition = getToolWithEdition();
+
+        List<IntellijJsonReleases> releases = jsonObj[i].getReleases();
+        for (IntellijJsonReleases r : releases) {
+          String version = r.getVersion();
+          Map<String, IntellijJsonDownloadsItem> downloads = r.getDownloads();
+          if (edition.getChild(version) == null) {
+            try {
+              UrlVersion urlVersion = edition.getOrCreateChild(version);
+              for (String os : downloads.keySet()) {
+                if (os.equals("windowsZip")) {
+                  addVersionEachOs(urlVersion, downloads, "windowsZip", OperatingSystem.WINDOWS,
+                      SystemArchitecture.X64);
+                } else if (os.equals("linux")) {
+                  addVersionEachOs(urlVersion, downloads, "linux", OperatingSystem.LINUX, SystemArchitecture.X64);
+                } else if (os.equals("mac")) {
+                  addVersionEachOs(urlVersion, downloads, "mac", OperatingSystem.MAC, SystemArchitecture.X64);
+                }
+              }
+              urlVersion.save();
+            } catch (Exception e) {
+              logger.error("For tool {} we failed to add version {}.", toolWithEdition, version, e);
+            }
+          }
+        }
+      }
     } catch (Exception e) {
-      throw new IllegalStateException("Failed to retrieve response body from url: " + url, e);
+      throw new IllegalStateException("Error while getting versions from JSON API " + JSON_URL, e);
     }
   }
 
   @Override
-  protected String getVersionUrl() {
+  protected void addVersion(UrlVersion urlVersion) {
 
-    return "https://www.jetbrains.com/idea/download/other.html";
+    throw new IllegalStateException();
   }
 
   @Override
-  protected Pattern getVersionPattern() {
+  protected String getEdition() {
 
-    return Pattern.compile("(\\d{4}\\.\\d\\.\\d)");
+    if (editionSwitch)
+      return "ultimate";
+    else
+      return "community";
   }
 
-  private WebDriver setupSelenium() {
+  private void addVersionEachOs(UrlVersion url, Map<String, IntellijJsonDownloadsItem> downloads, String json_os,
+      OperatingSystem os, SystemArchitecture systemArchitecture) {
 
-    WebDriverManager.chromedriver().setup();
-    ChromeOptions options = new ChromeOptions();
-    options.addArguments("--headless");
-    options.setExperimentalOption("excludeSwitches", new String[] { "enable-automation" });
-    options.addArguments("--disable-extensions");
-    options.addArguments("--disable-dev-shm-usage");
-    options.addArguments("--disable-browser-side-navigation");
-    options.addArguments("--disable-gpu");
-    options.addArguments("--no-sandbox");
-    options.addArguments("disable-infobars");
-    options.setHeadless(true);
-    return new ChromeDriver(options);
+    Map<String, Object> osValues = downloads.get(json_os).getOs_values();
+    String link = osValues.get("link").toString();
+    String checkSumLink = osValues.get("checksumLink").toString();
+    String cs = getCheckSum(checkSumLink);
+    doAddVersion(url, link, os, systemArchitecture, cs);
+
+  }
+
+  private String getCheckSum(String checksumLink) {
+
+    if (checksumLink != null && !checksumLink.isEmpty()) {
+      try {
+        URL url = new URL(checksumLink);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+
+        int responseCode = connection.getResponseCode();
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+          BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+          String response = null;
+          String line;
+
+          if ((line = reader.readLine()) != null) {
+            response = line.split(" ")[0];
+
+            reader.close();
+            connection.disconnect();
+
+            return response;
+          }
+        }
+      } catch (ProtocolException e) {
+        throw new RuntimeException(e);
+      } catch (MalformedURLException e) {
+        throw new RuntimeException(e);
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+
+    }
+    return null;
   }
 
   @Override
@@ -67,9 +147,19 @@ public abstract class IntellijUrlUpdater extends WebsiteUrlUpdater {
   }
 
   @Override
-  protected String mapVersion(String version) {
+  protected String doGetVersionUrl() {
 
-    // TODO remove this hack, see #1132
-    return super.mapVersion(version.replace("-", "."));
+    return JSON_URL;
+  }
+
+  @Override
+  protected Class<IntellijJsonObject> getJsonObjectType() {
+
+    return IntellijJsonObject.class;
+  }
+
+  @Override
+  protected void collectVersionsFromJson(IntellijJsonObject jsonItem, Collection<String> versions) {
+
   }
 }
