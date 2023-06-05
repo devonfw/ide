03d7392e0042170fe92b308a71afd2fdf7939f76
alfeilex <101652401+alfeilex@users.noreply.github.com>
2023-06-05 23:21:37 +0200

    #1131: Fix 404 downloads of url-updater (#1155)

--- url-updater/src/main/java/com/devonfw/tools/ide/url/updater/helm/HelmUrlUpdater.java
+++ url-updater/src/main/java/com/devonfw/tools/ide/url/updater/helm/HelmUrlUpdater.java
@@ -2,16 +2,25 @@ package com.devonfw.tools.ide.url.updater.helm;
 
 import com.devonfw.tools.ide.url.model.folder.UrlVersion;
 import com.devonfw.tools.ide.url.updater.GithubUrlUpdater;
+import com.devonfw.tools.ide.version.VersionIdentifier;
 
 /**
  * {@link GithubUrlUpdater} for "helm".
  */
 public class HelmUrlUpdater extends GithubUrlUpdater {
 
+  private static final VersionIdentifier MIN_MAC_ARM_VID = VersionIdentifier.of("3.4.0");
+
   @Override
   protected String getTool() {
 
     return "helm";
+  }
+
+  @Override
+  protected String getVersionPrefixToRemove() {
+
+    return "v";
   }
 
   @Override
@@ -23,11 +32,22 @@ public class HelmUrlUpdater extends GithubUrlUpdater {
   @Override
   protected void addVersion(UrlVersion urlVersion) {
 
+    VersionIdentifier vid = urlVersion.getVersionIdentifier();
+
     String baseUrl = "https://get.helm.sh/helm-${version}-";
     doAddVersion(urlVersion, baseUrl + "windows-amd64.zip", WINDOWS);
     doAddVersion(urlVersion, baseUrl + "linux-amd64.tar.gz", LINUX);
     doAddVersion(urlVersion, baseUrl + "darwin-amd64.tar.gz", MAC);
-    doAddVersion(urlVersion, baseUrl + "darwin-arm64.tar.gz", MAC, ARM64);
+    if (vid.compareVersion(MIN_MAC_ARM_VID).isGreater()) {
+      doAddVersion(urlVersion, baseUrl + "darwin-arm64.tar.gz", MAC, ARM64);
+    }
   }
+
+  @Override
+  protected String mapVersion(String version) {
+
+    return super.mapVersion("v" + version);
+  }
+
 
 }
--- url-updater/src/main/java/com/devonfw/tools/ide/url/updater/npm/NpmUrlUpdater.java
+++ url-updater/src/main/java/com/devonfw/tools/ide/url/updater/npm/NpmUrlUpdater.java
@@ -39,4 +39,5 @@ public class NpmUrlUpdater extends WebsiteUrlUpdater {
 
     return "npm-";
   }
+
 }
