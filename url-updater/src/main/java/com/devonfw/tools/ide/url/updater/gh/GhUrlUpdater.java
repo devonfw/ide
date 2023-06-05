e08bbf351ed08cfa78b4528712759c38fa0a3587
Jörg Hohwiller <hohwille@users.noreply.github.com>
2023-06-05 23:01:33 +0200

    use constant

--- url-updater/src/main/java/com/devonfw/tools/ide/url/updater/gh/GhUrlUpdater.java
+++ url-updater/src/main/java/com/devonfw/tools/ide/url/updater/gh/GhUrlUpdater.java
@@ -8,6 +8,9 @@ import com.devonfw.tools.ide.version.VersionIdentifier;
  * {@link GithubUrlUpdater} for "gh" (github CLI).
  */
 public class GhUrlUpdater extends GithubUrlUpdater {
+  
+  private static final VersionIdentifier MIN_MAC_ARM_VID = VersionIdentifier.of("2.23.0");
+
   @Override
   protected String getTool() {
 
@@ -24,7 +27,6 @@ public class GhUrlUpdater extends GithubUrlUpdater {
   protected void addVersion(UrlVersion urlVersion) {
 
     VersionIdentifier vid = urlVersion.getVersionIdentifier();
-    final VersionIdentifier MIN_MAC_ARM_VID = VersionIdentifier.of("2.23.0");
 
     String baseUrl = "https://github.com/cli/cli/releases/download/v${version}/gh_${version}_";
     doAddVersion(urlVersion, baseUrl + "windows_amd64.zip", WINDOWS, X64);
