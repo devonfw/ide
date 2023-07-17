1e6819437c1dd26d30ed0f21beca12abddf737e9
Laaouichi <oumaimalaaouichi10@gmail.com>
2023-07-17 11:46:12 +0200

    changes in PythonUrlUpdater

--- url-updater/src/main/java/com/devonfw/tools/ide/url/updater/python/PythonUrlUpdater.java
+++ url-updater/src/main/java/com/devonfw/tools/ide/url/updater/python/PythonUrlUpdater.java
@@ -1,5 +1,6 @@
 package com.devonfw.tools.ide.url.updater.python;
 
+import com.devonfw.tools.ide.common.OperatingSystem;
 import com.devonfw.tools.ide.json.mapping.JsonMapping;
 import com.devonfw.tools.ide.url.model.folder.UrlEdition;
 import com.devonfw.tools.ide.url.model.folder.UrlRepository;
@@ -82,8 +83,8 @@ public class PythonUrlUpdater extends JsonUrlUpdater<PythonJsonObject> {
                     try {
                         UrlVersion urlVersion = edition.getOrCreateChild(version);
                         for (PythonFiles download : result.getFiles()) {
-                            if (download.getPlatform().equals("win32")) {
-                                doAddVersion(urlVersion, download.getDownloadUrl());
+                            if (download.getPlatform().equals("win32") && download.getArch().equals("x64")) {
+                                doAddVersion(urlVersion, download.getDownloadUrl(), WINDOWS,X64,"");
                             } else if (download.getPlatform().equals("linux")) {
                                 doAddVersion(urlVersion, download.getDownloadUrl());
                             } else if (download.getPlatform().equals("darwin")) {
