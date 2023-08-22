23d9076c5c9ee40e332f3ac195aba181f0d5dcff
alfeilex <101652401+alfeilex@users.noreply.github.com>
2023-08-22 12:43:15 +0200

    Add JDK Version 17 and 20

--- ide/src/main/java/com/devonfw/tools/ide/url/updater/graalvm/GraalVmUrlUpdater.java
+++ ide/src/main/java/com/devonfw/tools/ide/url/updater/graalvm/GraalVmUrlUpdater.java
@@ -2,6 +2,7 @@ package com.devonfw.tools.ide.url.updater.graalvm;
 
 import com.devonfw.tools.ide.url.model.folder.UrlVersion;
 import com.devonfw.tools.ide.url.updater.GithubUrlUpdater;
+import com.devonfw.tools.ide.version.VersionIdentifier;
 
 /**
  * {@link GithubUrlUpdater} for GraalVM.
@@ -22,12 +23,19 @@ public class GraalVmUrlUpdater extends GithubUrlUpdater {
   @Override
   protected void addVersion(UrlVersion urlVersion) {
 
-    String baseUrl = "https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-${version}/graalvm-ce-java11-";
-    doAddVersion(urlVersion, baseUrl + "windows-amd64-${version}.zip", WINDOWS, X64);
-    doAddVersion(urlVersion, baseUrl + "linux-amd64-${version}.tar.gz", LINUX, X64);
-    doAddVersion(urlVersion, baseUrl + "darwin-amd64-${version}.tar.gz", MAC, X64);
-    doAddVersion(urlVersion, baseUrl + "darwin-aarch64-${version}.tar.gz", MAC, ARM64);
-
+    if(urlVersion.getVersionIdentifier().matches(VersionIdentifier.of("jdk"))) {
+      String baseUrl = "https://github.com/graalvm/graalvm-ce-builds/releases/download/${version}/graalvm-community-${version}_";
+      doAddVersion(urlVersion, baseUrl + "windows-x64_bin.zip", WINDOWS, X64);
+      doAddVersion(urlVersion, baseUrl + "linux-x64_bin.tar.gz", LINUX, X64);
+      doAddVersion(urlVersion, baseUrl + "macos-x64_bin.tar.gz", MAC, X64);
+      doAddVersion(urlVersion, baseUrl + "macos-aarch64_bin.tar.gz", MAC, ARM64);
+    } else {
+      String baseUrl = "https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-${version}/graalvm-ce-java11-";
+      doAddVersion(urlVersion, baseUrl + "windows-amd64-${version}.zip", WINDOWS, X64);
+      doAddVersion(urlVersion, baseUrl + "linux-amd64-${version}.tar.gz", LINUX, X64);
+      doAddVersion(urlVersion, baseUrl + "darwin-amd64-${version}.tar.gz", MAC, X64);
+      doAddVersion(urlVersion, baseUrl + "darwin-aarch64-${version}.tar.gz", MAC, ARM64);
+    }
   }
 
   @Override
