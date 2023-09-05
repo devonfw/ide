8238d7382be62689b17ab9538fbd9c880d1672cc
Jörg Hohwiller <joerg.hohwiller@capgemini.com>
2023-09-05 19:04:29 +0200

    #1090: improve ProcessContext, fixed readLine

--- ide/src/main/java/com/devonfw/tools/ide/process/ProcessContext.java
+++ ide/src/main/java/com/devonfw/tools/ide/process/ProcessContext.java
@@ -1,20 +1,22 @@
 package com.devonfw.tools.ide.process;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Wrapper for {@link ProcessBuilder} to simplify its usage and avoid common mistakes and pitfalls.
  */
 public interface ProcessContext {
 
-  /** Exit code for success. */
-  int SUCCESS = 0;
-
   /**
-   * @return the underlying {@link ProcessBuilder} instance.
+   * Exit code for success.
+   *
+   * @deprecated use {@link ProcessResult#SUCCESS}
    */
-  ProcessBuilder getProcessBuilder();
+  @Deprecated
+  int SUCCESS = 0;
 
   /**
    * @param handling the desired {@link ProcessErrorHandling}.
@@ -26,34 +28,195 @@ public interface ProcessContext {
    * @param directory the {@link Path} to the working directory from where to execute the command.
    * @return this {@link ProcessContext} for fluent API calls.
    */
-  default ProcessContext directory(Path directory) {
+  ProcessContext directory(Path directory);
 
-    getProcessBuilder().directory(directory.toFile());
+  /**
+   * Sets the executable command to be {@link #run()}.
+   *
+   * @param executable the {@link Path} to the command to be executed by {@link #run()}. Depending on your operating
+   *        system and the extension of the executable or OS specific conventions. So e.g. a *.cmd or *.bat file will be
+   *        called via CMD shell on windows while a *.sh file will be called via Bash, etc.
+   * @return this {@link ProcessContext} for fluent API calls.
+   */
+  ProcessContext executable(Path executable);
+
+  /**
+   * Sets the executable command to be {@link #run()}.
+   *
+   * @param executable the command to be executed by {@link #run()}.
+   * @return this {@link ProcessContext} for fluent API calls.
+   * @see #executable(Path)
+   */
+  default ProcessContext executable(String executable) {
+
+    return executable(Paths.get(executable));
+  }
+
+  /**
+   * Adds a single argument for {@link #executable(Path) command}.
+   *
+   * @param arg the next argument for {@link #executable(Path) command} to be added.
+   * @return this {@link ProcessContext} for fluent API calls.
+   */
+  ProcessContext addArg(String arg);
+
+  /**
+   * Adds a single argument for {@link #executable(Path) command}.
+   *
+   * @param arg the next argument for {@link #executable(Path) command} to be added.
+   * @return this {@link ProcessContext} for fluent API calls.
+   */
+  default ProcessContext addArg(Object arg) {
+
+    Objects.requireNonNull(arg);
+    return addArg(arg.toString());
+  }
+
+  /**
+   * Adds the given arguments for {@link #executable(Path) command}. E.g. for {@link #executable(Path) command} "mvn"
+   * the arguments "clean" and "install" may be added here to run "mvn clean install". If this method would be called
+   * again with "-Pmyprofile" and "-X" before {@link #run()} gets called then "mvn clean install -Pmyprofile -X" would
+   * be run.
+   *
+   * @param args the arguments for {@link #executable(Path) command} to be added.
+   * @return this {@link ProcessContext} for fluent API calls.
+   */
+  default ProcessContext addArgs(String... args) {
+
+    for (String arg : args) {
+      addArg(arg);
+    }
     return this;
+  }
+
+  /**
+   * Adds the given arguments for {@link #executable(Path) command} as arbitrary Java objects. It will add the
+   * {@link Object#toString() string representation} of these arguments to the command.
+   *
+   * @param args the arguments for {@link #executable(Path) command} to be added.
+   * @return this {@link ProcessContext} for fluent API calls.
+   */
+  default ProcessContext addArgs(Object... args) {
+
+    for (Object arg : args) {
+      addArg(arg);
+    }
+    return this;
+  }
+
+  /**
+   * Adds the given arguments for {@link #executable(Path) command} as arbitrary Java objects. It will add the
+   * {@link Object#toString() string representation} of these arguments to the command.
+   *
+   * @param args the {@link List} of arguments for {@link #executable(Path) command} to be added.
+   * @return this {@link ProcessContext} for fluent API calls.
+   */
+  default ProcessContext addArgs(List<?>... args) {
+
+    for (Object arg : args) {
+      addArg(arg);
+    }
+    return this;
+  }
+
+  /**
+   * Runs the previously configured {@link #executable(Path) command} with the configured {@link #addArgs(String...)
+   * arguments}. Will reset the {@link #addArgs(String...) arguments} but not the {@link #executable(Path) command} for
+   * sub-sequent calls.
+   *
+   * @return the exit code. Will be {@link #SUCCESS} on successful completion of the {@link Process}.
+   */
+  default int run() {
+
+    return run(false).getExitCode();
+  }
+
+  /**
+   * Runs the previously configured {@link #executable(Path) command} with the configured {@link #addArgs(String...)
+   * arguments}. Will reset the {@link #addArgs(String...) arguments} but not the {@link #executable(Path) command} for
+   * sub-sequent calls.
+   *
+   * @param capture - {@code true} to capture standard {@link ProcessResult#getOut() out} and
+   *        {@link ProcessResult#getErr() err} in the {@link ProcessResult}, {@code false} otherwise (to redirect out
+   *        and err).
+   * @return the {@link ProcessResult}.
+   */
+  ProcessResult run(boolean capture);
+
+  /**
+   * Runs the previously configured {@link #executable(Path) command} with the configured {@link #addArgs(String...)
+   * arguments}. Will reset the {@link #addArgs(String...) arguments} but not the {@link #executable(Path) command} for
+   * sub-sequent calls.
+   *
+   * @return a {@link List} with the standard output of the command, line by line.
+   * @deprecated use {@link #run(boolean)} instead.
+   */
+  @Deprecated
+  default List<String> runAndGetStdOut() {
+
+    return run(true).getOut();
   }
 
   /**
    * @param command the command to run.
    * @return the exit code. Will be {@link #SUCCESS} on successful completion of the {@link Process}.
+   * @deprecated use {@link #executable(Path)}, {@link #addArgs(String...)} and {@link #run()} instead.
    */
-  int run(String... command);
+  @Deprecated
+  default int run(String... command) {
+
+    executable(Paths.get(command[0]));
+    for (int i = 1; i < command.length; i++) {
+      addArg(command[i]);
+    }
+    return run();
+  }
 
   /**
    * @param commands the list of commands to run.
    * @return the exit code. Will be {@link #SUCCESS} on successful completion of the {@link Process}.
+   * @deprecated use {@link #executable(Path)}, {@link #addArgs(String...)} and {@link #run()} instead.
    */
-  int run(List<String> commands);
+  @Deprecated
+  default int run(List<String> commands) {
+
+    executable(Paths.get(commands.get(0)));
+    int size = commands.size();
+    for (int i = 1; i < size; i++) {
+      addArg(commands.get(i));
+    }
+    return run();
+  }
 
   /**
    * @param command the command to run.
    * @return a {@link List} with the standard output of the command, line by line.
+   * @deprecated use {@link #executable(Path)}, {@link #addArgs(String...)} and {@link #runAndGetStdOut()} instead.
    */
-  List<String> runAndGetStdOut(String... command);
+  @Deprecated
+  default List<String> runAndGetStdOut(String... command) {
+
+    executable(Paths.get(command[0]));
+    for (int i = 1; i < command.length; i++) {
+      addArg(command[i]);
+    }
+    return runAndGetStdOut();
+  }
 
   /**
    * @param commands the list of commands to run.
    * @return a {@link List} with the standard output of the command, line by line.
+   * @deprecated use {@link #executable(Path)}, {@link #addArgs(String...)} and {@link #runAndGetStdOut()} instead.
    */
-  List<String> runAndGetStdOut(List<String> commands);
+  @Deprecated
+  default List<String> runAndGetStdOut(List<String> commands) {
+
+    executable(Paths.get(commands.get(0)));
+    int size = commands.size();
+    for (int i = 1; i < size; i++) {
+      addArg(commands.get(i));
+    }
+    return runAndGetStdOut();
+  }
 
 }
--- ide/src/main/java/com/devonfw/tools/ide/process/ProcessContextImpl.java
+++ ide/src/main/java/com/devonfw/tools/ide/process/ProcessContextImpl.java
@@ -3,43 +3,54 @@ package com.devonfw.tools.ide.process;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.lang.ProcessBuilder.Redirect;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 import com.devonfw.tools.ide.cli.CliException;
-import com.devonfw.tools.ide.log.IdeLogger;
+import com.devonfw.tools.ide.context.IdeContext;
+import com.devonfw.tools.ide.environment.VariableLine;
 import com.devonfw.tools.ide.log.IdeSubLogger;
+import com.devonfw.tools.ide.util.FilenameUtil;
 
 /**
  * Implementation of {@link ProcessContext}.
  */
 public final class ProcessContextImpl implements ProcessContext {
 
+  private final IdeContext context;
+
   private final ProcessBuilder processBuilder;
 
-  private final IdeLogger logger;
+  private final List<String> arguments;
+
+  private Path executable;
 
   private ProcessErrorHandling errorHandling;
 
   /**
    * The constructor.
    *
-   * @param processBuilder the {@link ProcessBuilder}.
-   * @param logger the {@link IdeLogger}.
+   * @param context the owning {@link IdeContext}.
    */
-  public ProcessContextImpl(ProcessBuilder processBuilder, IdeLogger logger) {
+  public ProcessContextImpl(IdeContext context) {
 
     super();
-    this.processBuilder = processBuilder;
-    this.logger = logger;
+    this.context = context;
+    this.processBuilder = new ProcessBuilder();
+    // TODO needs to be configurable for GUI
+    this.processBuilder.redirectOutput(Redirect.INHERIT).redirectError(Redirect.INHERIT);
     this.errorHandling = ProcessErrorHandling.THROW;
-  }
-
-  @Override
-  public ProcessBuilder getProcessBuilder() {
-
-    return this.processBuilder;
+    Map<String, String> environment = this.processBuilder.environment();
+    for (VariableLine var : this.context.getVariables().collectVariables()) {
+      if (var.isExport()) {
+        environment.put(var.getName(), var.getValue());
+      }
+    }
+    this.arguments = new ArrayList<>();
   }
 
   @Override
@@ -51,118 +62,134 @@ public final class ProcessContextImpl implements ProcessContext {
   }
 
   @Override
-  public int run(String... command) {
+  public ProcessContext directory(Path directory) {
 
-    return runCommand(null, command);
+    this.processBuilder.directory(directory.toFile());
+    return this;
   }
 
   @Override
-  public int run(List<String> commands) {
+  public ProcessContext executable(Path command) {
 
-    return runCommand(null, commands);
-  }
-
-  @Override
-  public List<String> runAndGetStdOut(String... command) {
-
-    List<String> out = new ArrayList<>();
-    runCommand(out, command);
-    return out;
-  }
-
-  @Override
-  public List<String> runAndGetStdOut(List<String> command) {
-
-    List<String> out = new ArrayList<>();
-    runCommand(out, command);
-    return out;
-  }
-  
-  private int runCommand(List<String> out, List<String> commands){
-    if ((commands == null) || (commands.isEmpty())) {
-      throw new IllegalArgumentException("Commands must not be empty!");
-    }
-
-    this.processBuilder.command(commands);
-    return processCommand(out, commands.toString());
-  }
-
-  private int runCommand(List<String> out, String... command) {
-
-    if ((command == null) || (command.length < 1) || command[0].isBlank()) {
-      throw new IllegalArgumentException("Command must not be empty!");
-    }
-
-    this.processBuilder.command(command);
-    return processCommand(out, command);
-  }
-
-  private int processCommand(List<String> out, String... command) {
-
-    if (this.logger.debug().isEnabled()) {
-      String message = createCommandMessage(" ...", command);
-      this.logger.debug(message);
-    }
-
-    try {
-      if (out == null) {
-        this.processBuilder.redirectOutput(Redirect.INHERIT);
-      } else {
-        this.processBuilder.redirectOutput(Redirect.PIPE);
-      }
-      Process process = this.processBuilder.start();
-      int exitCode;
-      if (out == null) {
-        exitCode = process.waitFor();
-      } else {
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
-          while (true) {
-            String line = reader.readLine();
-            if (line == null) {
-              break;
+    Path exe = command;
+    if (exe.isAbsolute()) {
+      Path parent = command.getParent();
+      String filename = command.getFileName().toString();
+      String extension = FilenameUtil.getExtension(filename);
+      if (extension == null) {
+        if (this.context.getSystemInfo().isWindows()) {
+          Path cmd = parent.resolve(filename + ".cmd");
+          if (Files.exists(cmd)) {
+            exe = cmd;
+          } else {
+            cmd = parent.resolve(filename + ".bat");
+            if (Files.exists(cmd)) {
+              exe = cmd;
             }
-            out.add(line);
           }
         }
-        exitCode = process.exitValue();
       }
-      if ((exitCode != SUCCESS) && (this.errorHandling != ProcessErrorHandling.NONE)) {
-        String message = createCommandMessage(" failed with exit code " + exitCode + "!", command);
+    }
+    if (!exe.equals(command)) {
+      this.context.debug("Using " + exe.getFileName() + " for " + command);
+    }
+    this.executable = exe;
+    return this;
+  }
+
+  @Override
+  public ProcessContext addArg(String arg) {
+
+    this.arguments.add(arg);
+    return this;
+  }
+
+  @Override
+  public ProcessResult run(boolean capture) {
+
+    if (this.executable == null) {
+      throw new IllegalStateException("Missing executable to run process!");
+    }
+    // pragmatic solution to avoid copying lists/arrays
+    this.arguments.add(0, this.executable.toString());
+    this.processBuilder.command(this.arguments);
+    if (this.context.debug().isEnabled()) {
+      String message = createCommandMessage(" ...");
+      this.context.debug(message);
+    }
+    try {
+      if (capture) {
+        this.processBuilder.redirectOutput(Redirect.PIPE).redirectError(Redirect.PIPE);
+      }
+      List<String> out = null;
+      List<String> err = null;
+      Process process = this.processBuilder.start();
+      if (capture) {
+        out = new ArrayList<>();
+        err = new ArrayList<>();
+        try (BufferedReader outReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            BufferedReader errReader = new BufferedReader(new InputStreamReader(process.getErrorStream()))) {
+          String outLine = "";
+          String errLine = "";
+          while ((outLine != null) || (errLine != null)) {
+            if (outLine != null) {
+              outLine = outReader.readLine();
+              if (outLine != null) {
+                out.add(outLine);
+              }
+            }
+            if (errLine != null) {
+              errLine = errReader.readLine();
+              if (errLine != null) {
+                err.add(errLine);
+              }
+            }
+          }
+        }
+      }
+      int exitCode = process.waitFor();
+      ProcessResult result = new ProcessResultImpl(exitCode, out, err);
+      if (!result.isSuccessful() && (this.errorHandling != ProcessErrorHandling.NONE)) {
+        String message = createCommandMessage(" failed with exit code " + exitCode + "!");
         if (this.errorHandling == ProcessErrorHandling.THROW) {
           throw new CliException(message, exitCode);
         }
         IdeSubLogger level;
         if (this.errorHandling == ProcessErrorHandling.ERROR) {
-          level = this.logger.error();
+          level = this.context.error();
         } else if (this.errorHandling == ProcessErrorHandling.WARNING) {
-          level = this.logger.warning();
+          level = this.context.warning();
         } else {
-          level = this.logger.error();
+          level = this.context.error();
           level.log("Internal error: Undefined error handling {}", this.errorHandling);
         }
         level.log(message);
       }
-      return exitCode;
+      return result;
     } catch (Exception e) {
       String msg = e.getMessage();
       if ((msg == null) || msg.isEmpty()) {
         msg = e.getClass().getSimpleName();
       }
-      throw new IllegalStateException(createCommandMessage(" failed: " + msg, command), e);
+      throw new IllegalStateException(createCommandMessage(" failed: " + msg), e);
+    } finally {
+      this.arguments.clear();
     }
   }
 
-  private String createCommandMessage(String suffix, String... command) {
+  private String createCommandMessage(String suffix) {
 
     StringBuilder sb = new StringBuilder();
     sb.append("Running command '");
-    sb.append(command[0]);
+    sb.append(this.executable);
     sb.append("'");
-    if (command.length > 1) {
+    int size = this.arguments.size();
+    if (size > 1) {
       sb.append(" with arguments");
-      for (int i = 1; i < command.length; i++) {
+      for (int i = 1; i < size; i++) {
+        String arg = this.arguments.get(i);
         sb.append(" '");
-        sb.append(command[i]);
+        sb.append(arg);
         sb.append("'");
       }
     }
